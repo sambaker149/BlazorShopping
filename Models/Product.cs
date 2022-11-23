d1b712dd5aff08ed554ca1b4c401430606dec357
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2022-11-23 16:03:35 +0000

    Created Product Model, ProductService, and ProductList Razor Component

---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShopping.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; } = 0;
+
+        [Required, StringLength(60)]
+        public string Name { get; set; } = String.Empty;
+
+        [StringLength(255)]
+        public string? Description { get; set; } = String.Empty;
+
+        [Required, Range(0,100.00)]
+        public decimal Price { get; set; } = 0;
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using BlazorShopping.Models;
+
+namespace BlazorShopping.Services
+{
+    public class ProductService
+    {
+        public static readonly List<Product> Products = new List<Product>()
+        {
+            new()
+            {
+                Id = 1,
+                Name = "P1",
+                Description = "Example Product",
+                Price = 70
+            }
+        };
+    }
+}
